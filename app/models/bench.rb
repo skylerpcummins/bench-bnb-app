2f27c5871d37bda31b8f608963ee86f52b2ee243
Skyler Cummins <skylerpcummins@gmail.com>
2015-12-11 17:04:28 -0700

    start bench#in_bounds

--- app/models/bench.rb
+++ app/models/bench.rb
@@ -2,7 +2,14 @@ class Bench < ActiveRecord::Base
   validates :description, :lat, :lng, presence: true
 
   def self.in_bounds(bounds)
-  end
+      # bounds in the following format:
+     # {
+     #   "northEast"=> {"lat"=>"37.80971", "lng"=>"-122.39208"},
+     #   "southWest"=> {"lat"=>"37.74187", "lng"=>"-122.47791"}
+     # }
 
+     # return self.lat.between?(bounds[southWest[lat]], bounds[northEast][lat])
+
+  end
 
 end
